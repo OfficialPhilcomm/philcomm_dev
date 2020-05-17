6e7877661dcf25089069e481ee491c6cdd5206eb
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-17 22:08:56 +0200

    Add all orders request

--- servicemarket/js/BackendAPI.js
+++ servicemarket/js/BackendAPI.js
@@ -27,6 +27,20 @@ class BackendAPI {
     }
   }
 
+  static allOrders() {
+    const url = "api/all_orders.php";
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, false);
+    request.send(null);
+
+    if (request.status === 200) {
+      let apiResponse = JSON.parse(request.responseText);
+
+      console.log(apiResponse);
+    }
+  }
+
   static login(user, pass) {
     const url = "api/login.php";
 
