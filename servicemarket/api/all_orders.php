8713773c9590105eee54a12fa47b04f13bc4c5d6
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-17 21:52:22 +0200

    Add all orders endpoint

--- servicemarket/api/all_orders.php
+++ servicemarket/api/all_orders.php
@@ -9,15 +9,25 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$stmt = $conn->prepare("select * from UserOrder where UserID = ?");
+$api_result = new stdClass();
+$api_result->type = "my_orders";
+$api_result->orders = array();
+
+$stmt = $conn->prepare("select uo.ID as ID, u.Username as Username from UserOrder uo
+join User u on uo.UserID = u.ID");
 $stmt->bind_param("i", getUserID());
 $stmt->execute();
 
 $result = $stmt->get_result();
 if($result->num_rows === 0) exit('No rows');
 while($row = $result->fetch_assoc()) {
-  echo $row['ID'] . "\n";
+  $order = new stdClass();
+  $order->id = $row["ID"];
+  $order->user_name = $row["Username"];
+  $api_result->orders[] = $order;
 }
+
+echo json_encode($api_result);
 
 $stmt->close();
 
