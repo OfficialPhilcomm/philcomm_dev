1361406ac103cfa2c9253351076dccc854aefe68
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-17 21:37:22 +0200

    Add username to result

--- servicemarket/api/all_orders.php
+++ servicemarket/api/all_orders.php
@@ -9,25 +9,15 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$api_result = new stdClass();
-$api_result->type = "my_orders";
-$api_result->orders array();
-
-$stmt = $conn->prepare("
-select uo.ID as ID, u.Username as Username from UserOrder uo on uo.UserID = ?
-join User u on uo.UserID = u.ID");
+$stmt = $conn->prepare("select * from UserOrder where UserID = ?");
 $stmt->bind_param("i", getUserID());
 $stmt->execute();
 
 $result = $stmt->get_result();
 if($result->num_rows === 0) exit('No rows');
 while($row = $result->fetch_assoc()) {
-  $order->id = $row["ID"];
-  $order->user_name = $row["Username"]
-  array_push($api_result->orders, $order);
+  echo $row['ID'] . "\n";
 }
-
-echo json_encode($api_result);
 
 $stmt->close();
 
