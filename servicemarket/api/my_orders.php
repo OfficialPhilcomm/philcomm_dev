b08bdc194db73a1949e60fd259c6cc71318c97f3
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-17 20:42:33 +0200

    Rework my orders endpoint

--- servicemarket/api/my_orders.php
+++ servicemarket/api/my_orders.php
@@ -1,21 +1,20 @@
 <?php
 include 'general.php';
 
+requireLogin();
+
 $conn = new mysqli(db_host(), db_user(), db_pass(), db_name());
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$stmt = $conn->prepare("select * from Order");
-$stmt->execute();
+$sql = "select * from Order";
 
-$result = $stmt->get_result();
+$result = $conn->query($sql);
 if($result->num_rows === 0) exit('No rows');
 while($row = $result->fetch_assoc()) {
   echo $row['ID'];
 }
-
-$stmt.close();
 
 $conn->close();
