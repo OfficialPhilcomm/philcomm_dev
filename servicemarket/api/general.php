0aa3af2b7ff099300d32d3d49c28c7beb816a315
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-18 17:03:00 +0200

    Fix php syntax error

--- servicemarket/api/general.php
+++ servicemarket/api/general.php
@@ -19,9 +19,9 @@ function getUserID() {
   return $_SESSION['user_id'];
 }
 
-function throwError(error) {
+function throwError($error) {
   $api_response = new stdClass();
   $api_response->errors = array();
-  $api_response->errors[] = error;
+  $api_response->errors[] = $error;
   exit(json_encode($api_response));
 }
