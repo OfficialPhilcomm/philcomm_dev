6313a348333324e071117eaf51035f80c7d486c6
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-19 22:02:56 +0200

    Add new requests

---
+++ servicemarket/api/accept_offer.php
@@ -0,0 +1,36 @@
+<?php
+include 'general.php';
+
+requireLogin();
+
+$body = json_decode(file_get_contents('php://input'));
+
+$conn = new mysqli(db_host(), db_user(), db_pass(), db_name());
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$stmt = $conn->prepare("select ID from UserOrder uo
+join Offer o on uo.ID = ?
+and uo.UserID = ?
+and uo.AcceptedOfferID is null
+and uo.ID = o.UserOrderID
+and o.ID = ?");
+$stmt->bind_param("ii", $body->user_order_id, getUserID(), $body->offer_id);
+$stmt->execute();
+$result = $stmt->get_result();
+if($result->num_rows === 0) throwError("no valid order found");
+
+$user_order_id = $body->user_order_id;
+$offer_id = $body->offer_id;
+
+$stmt = $conn->prepare("update UserOrder
+set AcceptedOfferID = ?, State = 'started' where ID = ?");
+
+$stmt->bind_param("ii", $offer_id, getUserID(), $user_order_id);
+$stmt->execute();
+
+$stmt->close();
+
+$conn->close();
---
+++ servicemarket/api/new_offer.php
@@ -0,0 +1,29 @@
+<?php
+include 'general.php';
+
+requireLogin();
+
+$body = json_decode(file_get_contents('php://input'));
+
+$conn = new mysqli(db_host(), db_user(), db_pass(), db_name());
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$stmt = $conn->prepare("select ID from UserOrder
+where ID = ?
+and UserID != ?
+and AcceptedOfferID is null");
+$stmt->bind_param("ii", $body->user_order_id, getUserID());
+$stmt->execute();
+$result = $stmt->get_result();
+if($result->num_rows === 0) throwError("no valid order found");
+
+$stmt = $conn->prepare("insert into Offer (Price,UserID,OrderDataID) values (?, ?, ?)");
+$stmt->bind_param("iii", $body->price, getUserID(), $orderDataID);
+$stmt->execute();
+
+$stmt->close();
+
+$conn->close();
