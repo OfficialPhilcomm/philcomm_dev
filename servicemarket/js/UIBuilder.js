13c8c0f5f9d25ddda9998b6615b48b894386ba79
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-19 21:06:28 +0200

    Add content ability

--- servicemarket/js/UIBuilder.js
+++ servicemarket/js/UIBuilder.js
@@ -7,6 +7,7 @@ class UIBuilder {
     if(uiJSON.id) domElement.id = uiJSON.id;
     if(uiJSON.class) domElement.className = uiJSON.class;
     if(uiJSON.src) domElement.src = uiJSON.src;
+    if(uiJSON.content) domElement.innerHTML = uiJSON.content;
     if(uiJSON.children) {
       for(let child of children) {
         domElement.appendChild(fromObject(child));
