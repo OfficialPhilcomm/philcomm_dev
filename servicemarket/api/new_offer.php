48987d47022558ff5f6e580f24c04b2ffe94ade5
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-20 17:21:20 +0200

    Remove logs

--- servicemarket/api/new_offer.php
+++ servicemarket/api/new_offer.php
@@ -16,25 +16,13 @@ where ID = ?
 and UserID != ?
 and AcceptedOfferID is null");
 $stmt->bind_param("ii", $body->user_order_id, getUserID());
-echo "UserID: ".getUserID()."\n";
-echo "order id: ".$body->user_order_id."\n";
-echo "price: ".$body->price;
 $stmt->execute();
 $result = $stmt->get_result();
 if($result->num_rows === 0) throwError("no valid order found");
 
-echo "\n\n"."insert into Offer (Price,UserID,UserOrderID) values (".$body->price.", ".getUserID().", ".$body->user_order_id.")";
-
 $stmt = $conn->prepare("insert into Offer (Price,UserID,UserOrderID) values (?, ?, ?)");
-if($stmt === false) {
-  echo "prepare failed: ".$conn->error;
-}
-if($stmt->bind_param("iii", $body->price, getUserID(), $body->user_order_id) === false) {
-  echo "binding failed";
-}
+$stmt->bind_param("iii", $body->price, getUserID(), $body->user_order_id)
 $stmt->execute();
-echo "execute error".$stmt->error;
-echo "type: ";
 
 $stmt->close();
 
