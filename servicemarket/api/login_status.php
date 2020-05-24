15be47da644b65eb79166d4537dcb1a442e3af67
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-24 10:38:53 +0200

    Add login status endpoint

---
+++ servicemarket/api/login_status.php
@@ -0,0 +1,14 @@
+<?php
+include 'general.php';
+
+$api_response = new stdClass();
+if(is_set($_SESSION['valid_login'])) {
+  $api_response->logged_in = $_SESSION['valid_login'];
+  if($_SESSION['valid_login']) {
+    $api_response->username = $_SESSION['username'];
+  }
+} else {
+  $api_response->logged_in = false;
+}
+
+exit(json_encode($api_response));
