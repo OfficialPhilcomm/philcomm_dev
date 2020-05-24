e9c391852aba7cfbb74f54dd23ae7770907013cc
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-24 15:57:01 +0200

    Add iv and ev validation

--- servicemarket/api/new_order.php
+++ servicemarket/api/new_order.php
@@ -30,7 +30,42 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$stmt = $conn->prepare("insert into OrderData (PokemonName, Gender, Move1, Move2, Move3, Move4, Ability)
+IVHP int not null default -1,
+IVATK int not null default -1,
+IVDEF int not null default -1,
+IVSPATK int not null default -1,
+IVSPDEF int not null default -1,
+IVSPE int not null default -1,
+EVHP int not null default -1,
+EVATK int not null default -1,
+EVDEF int not null default -1,
+EVSPATK int not null default -1,
+EVSPDEF int not null default -1,
+EVSPE int not null default -1
+
+$ivs = array();
+$ivs[] = "iv_hp";
+$ivs[] = "iv_atk";
+$ivs[] = "iv_def";
+$ivs[] = "iv_spatk";
+$ivs[] = "iv_spdef";
+$ivs[] = "iv_spe";
+$evs = array();
+$evs[] = "ev_hp";
+$evs[] = "ev_atk";
+$evs[] = "ev_def";
+$evs[] = "ev_spatk";
+$evs[] = "ev_spdef";
+$evs[] = "ev_spe";
+
+foreach($ivs as $value) {
+  if($body[$value] < -1 || $body[$value] > 31) throwError("$value must be between -1 and 31");
+}
+foreach($evs as $value) {
+  if($body[$value] < 0 || $body[$value] > 252) throwError("$value must be between -1 and 31");
+}
+
+$stmt = $conn->prepare("insert into OrderData (PokemonName, Gender, Move1, Move2, Move3, Move4, Ability, )
 values (?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("sssssss", $body->pokemon_name, $body->gender, $body->move1, $body->move2, $body->move3, $body->move4, $body->ability);
 $stmt->execute();
