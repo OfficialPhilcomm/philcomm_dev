0916ba5a4a727b82504990a1b1098672da62d7c5
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-24 16:35:20 +0200

    Fix endpoint

--- servicemarket/api/new_order.php
+++ servicemarket/api/new_order.php
@@ -69,10 +69,9 @@ $stmt = $conn->prepare("insert into OrderData (PokemonName, Gender, Move1, Move2
 IVHP, IVATK, IVDEF, IVSPATK, IVSPDEF, IVSPE, EVHP, EVATK, EVDEF, EVSPATK, EVSPDEF, EVSPE)
 values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("sssssssiiiiiiiiiiii", $body->pokemon_name, $body->gender, $body->move1, $body->move2, $body->move3, $body->move4, $body->ability,
-$body->iv_hp, $body->iv_atk, $body->iv_def, $body->iv_spatk, $body->iv_spdev, $body->iv_spe,
-$body->ev_hp, $body->ev_atk, $body->ev_def, $body->ev_spatk, $body->ev_spdev, $body->ev_spe);
+$body->iv_hp, $body->iv_atk, $body->iv_def, $body->iv_spatk, $body->iv_spdef, $body->iv_spe,
+$body->ev_hp, $body->ev_atk, $body->ev_def, $body->ev_spatk, $body->ev_spdef, $body->ev_spe);
 $stmt->execute();
-throwError($stmt->error);
 
 $userID = getUserID();
 $orderDataID = $conn->insert_id;
