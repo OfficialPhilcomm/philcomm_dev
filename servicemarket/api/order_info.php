7a6be1b0d3159135f01b6e37a97bd8023d2da85c
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-24 13:47:59 +0200

    Add pokemon name to response

--- servicemarket/api/order_info.php
+++ servicemarket/api/order_info.php
@@ -15,11 +15,12 @@ if ($conn->connect_error) {
 $api_result = new stdClass();
 $api_result->type = "order_info";
 
-$stmt = $conn->prepare("select uo.State as State, u.Username as Breeder, o.Price as Price from UserOrder uo
+$stmt = $conn->prepare("select od.PokemonName as PokemonName, uo.State as State, u.Username as Breeder, o.Price as Price from UserOrder uo
 join Offer o on uo.ID = ?
 and uo.UserID = ?
 and uo.AcceptedOfferID = o.ID
-join User u on o.UserID = u.ID");
+join User u on o.UserID = u.ID
+join OrderData od on uo.OrderDataID = od.id");
 $stmt->bind_param("ii", $body->user_order_id, getUserID());
 $stmt->execute();
 
@@ -27,6 +28,7 @@ $result = $stmt->get_result();
 if($result->num_rows === 0) throwError("no valid order found");
 while($row = $result->fetch_assoc()) {
   $order = new stdClass();
+  $order->pokemon_name = $row["PokemonName"];
   $order->state = $row["State"];
   $order->breeder = $row["Breeder"];
   $order->price = $row["Price"];
