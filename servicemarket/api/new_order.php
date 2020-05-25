d596a9d277cdcaa0fae9e7acf7e9c40abdc7fe87
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-05-25 21:57:03 +0200

    Make level a requirement

--- servicemarket/api/new_order.php
+++ servicemarket/api/new_order.php
@@ -9,6 +9,7 @@ $arguments['move1'] = 'string';
 $arguments['move2'] = 'string';
 $arguments['move3'] = 'string';
 $arguments['move4'] = 'string';
+$arguments['level'] = 'integer';
 $arguments['ability'] = 'string';
 $arguments['iv_hp'] = 'integer';
 $arguments['iv_atk'] = 'integer';
@@ -45,6 +46,7 @@ $evs[] = "ev_spatk";
 $evs[] = "ev_spdef";
 $evs[] = "ev_spe";
 
+if($body->level < 0 || $body->level > 100) throwError("level must be between 0 and 100");
 foreach($ivs as $value) {
   if($body->$value < -1 || $body->$value > 31) throwError("$value must be between -1 and 31");
 }
@@ -55,10 +57,10 @@ foreach($evs as $value) {
 $total_evs = $body->ev_hp + $body->ev_atk + $body->ev_def + $body->ev_spatk + $body->ev_spdef + $body->ev_spe;
 if($total_evs > 510) throwError("total evs cant be bigger than 510");
 
-$stmt = $conn->prepare("insert into OrderData (PokemonName, Gender, Move1, Move2, Move3, Move4, Ability,
+$stmt = $conn->prepare("insert into OrderData (PokemonName, Gender, Level, Move1, Move2, Move3, Move4, Ability,
 IVHP, IVATK, IVDEF, IVSPATK, IVSPDEF, IVSPE, EVHP, EVATK, EVDEF, EVSPATK, EVSPDEF, EVSPE)
 values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("sssssssiiiiiiiiiiii", $body->pokemon_name, $body->gender, $body->move1, $body->move2, $body->move3, $body->move4, $body->ability,
+$stmt->bind_param("ssisssssiiiiiiiiiiii", $body->pokemon_name, $body->gender, $body->level, $body->move1, $body->move2, $body->move3, $body->move4, $body->ability,
 $body->iv_hp, $body->iv_atk, $body->iv_def, $body->iv_spatk, $body->iv_spdef, $body->iv_spe,
 $body->ev_hp, $body->ev_atk, $body->ev_def, $body->ev_spatk, $body->ev_spdef, $body->ev_spe);
 $stmt->execute();
