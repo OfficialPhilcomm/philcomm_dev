baf13e6463b0cc90f8db3293cd815ea9c51393ea
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-06-28 13:20:56 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>Page not found | @dev_philcomm</title>
+
+  <meta charset="utf-8">
+  <meta name="description" content="404 not found">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/404.css">
+</head>
+<body>
+  <main>
+    The given URL was not found
+  </main>
+
+  <?php include 'assets/components/footer.html' ?>
+</body>
+</html>
