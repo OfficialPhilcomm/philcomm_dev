d72a7cc26fe470c44c28172211c23effe50ae19d
Philipp Schlesinger <officialphilcomm@gmail.com>
2020-07-12 14:47:13 +0200

    Add username pattern

--- servicemarket/js/main.js
+++ servicemarket/js/main.js
@@ -212,7 +212,7 @@ loggedIn.registerListener(function(newValue) {
     let notRegisteredButton = UIBuilder.fromObject({type: 'div', class: 'clickable', content: 'No Account? Create one here'});
 
     let registerEmailInput = UIBuilder.fromObject({type: 'input', input_type: 'email', required: true});
-    let registerUsernameInput = UIBuilder.fromObject({type: 'input'});
+    let registerUsernameInput = UIBuilder.fromObject({type: 'input', input_pattern: '.{1,}'});
     let registerPasswordInput = UIBuilder.fromObject({type: 'input', input_type: 'password'});
     let registerPasswordRepeatInput = UIBuilder.fromObject({type: 'input', input_type: 'password'});
     let registerErrorDisplay = UIBuilder.fromObject({type: 'div'});
