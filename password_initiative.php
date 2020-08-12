748668bac200dd8bf71d07a688267fdbba92a116
Philipp Schlesinger <info@philcomm.dev>
2020-08-12 14:26:48 +0200

    Add password initiative v1

---
+++ password_initiative.php
@@ -0,0 +1,38 @@
+<?php include 'helper.php' ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php print_meta_tags(
+      "homepage, development, safety, passwords",
+      "save password initiative",
+      "An initiative motivating people to use save passwords",
+      "https://philcomm.dev/flappybird")
+    ?>
+
+    <link rel="stylesheet" href="assets/css/footer.css">
+  </head>
+  <body>
+    <main>
+      <div class="text-section">
+        <h1>save password initiative</h1>
+        <p>
+          Everybody knows that internet privacy is important. Today, we are doing
+          so much online, that perhaps losing the key to your home is not as harmful
+          as getting your passwords leaked.
+        </p>
+        <p>
+          That's why it is very important to have safe passwords. But just your name
+          with a number and one capital letter doesn't do the trick.
+        </p>
+        <p>
+          A lot of people don't use save passwords because they fear forgetting them, me included.
+          So I came up with a system that works good for me, and it might help other people too.
+          That's why I am sharing it here.
+        </p>
+      </div>
+    </main>
+
+    <?php include 'assets/components/footer.html' ?>
+  </body>
+</html>
