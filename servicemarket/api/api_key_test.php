a0609f78cfc92dafed5a89dd86d7eb3581f8750a
Philipp Schlesinger <info@philcomm.dev>
2020-08-19 12:30:41 +0200

    Add user id printing

--- servicemarket/api/api_key_test.php
+++ servicemarket/api/api_key_test.php
@@ -1,6 +1,8 @@
 <?php
 include 'general.php';
 
-// curl -H "API-Token: test" -H "Auth-Token: test2" https://philcomm.dev/servicemarket/api/api_key_test.php
+// curl -H "API-Token: test" -H "Auth-Token: 0357625e-e205-11ea-af0d-001a4a150180" https://philcomm.dev/servicemarket/api/api_key_test.php
 
 requireLogin();
+
+echo "User ID: ".getUserID();
