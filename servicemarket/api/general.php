05b67ab5962117ac21e64dae248ae21ee7b5c43a
Philipp Schlesinger <info@philcomm.dev>
2020-08-19 11:44:57 +0200

    Fix headers

--- servicemarket/api/general.php
+++ servicemarket/api/general.php
@@ -13,21 +13,17 @@ function db_name() { return "dbs443745"; }
 function requireLogin() {
   $headers = apache_request_headers();
 
-  foreach ($headers as $header => $value) {
-    echo "$header: $value\n";
-  }
-
-  /*if(!array_key_exists("API-Key", $headers)) {
-    throwError("API-Key header not set; please request active api key");
+  if(!array_key_exists("API-Token", $headers)) {
+    throwError("API-Token header not set; please request active api key");
   }
 
   if(!array_key_exists("Auth-Token", $headers)) {
     throwError("Auth-Token header not set; please use active user token");
-  }*/
+  }
 
-  $api_token = $headers['API-Key'];
+  $api_token = $headers['API-Token'];
   $auth_token = $headers['Auth-Token'];
-  echo "API-Key: ".$api_token;
+  echo "API-Token: ".$api_token;
   echo "Auth-Token: ".$auth_token;
 }
 
