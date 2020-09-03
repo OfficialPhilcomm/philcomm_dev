a05a71a63c6cf391722cb714558a5c0eaa826e24
Philipp Schlesinger <info@philcomm.dev>
2020-09-03 19:38:30 +0200

    Return success true when order creation is successful

--- servicemarket/api/orders/new_order.php
+++ servicemarket/api/orders/new_order.php
@@ -29,7 +29,7 @@ $body = validateBody($arguments);
 $conn = new mysqli(db_host(), db_user(), db_pass(), db_name());
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  throwError("db error");
 }
 
 $ivs = array();
@@ -77,3 +77,7 @@ $stmt->execute();
 $stmt->close();
 
 $conn->close();
+
+$api_response = new stdClass();
+$api_response->success = true;
+exit(json_encode($api_response));
