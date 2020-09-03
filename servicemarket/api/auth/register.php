077529753871ee8f1cb95163e3785e4d2dbf757a
Philipp Schlesinger <info@philcomm.dev>
2020-09-03 14:50:15 +0200

    Modify registering for alpha

--- servicemarket/api/auth/register.php
+++ servicemarket/api/auth/register.php
@@ -1,24 +1,31 @@
 <?php
 include '../general.php';
 
-$arguments['email'] = 'string';
 $arguments['username'] = 'string';
 $arguments['password'] = 'string';
 $body = validateBody($arguments);
 
+if(strlen($body->password) < 8) throwError("password must be 8 characters");
+
 $conn = new mysqli(db_host(), db_user(), db_pass(), db_name());
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  throwError("db error");
 }
 
-$stmt = $conn->prepare("insert into User (Email, Username, Password) values (?, ?, password(?))");
-$stmt->bind_param("sss", $body->email, $body->username, $body->password);
+$stmt = $conn->prepare("select ID from User where Username = ?");
+$stmt->bind_param("s", $body->username);
+$stmt->execute();
+$resukt = $stmt->get_result();
+if($result->num_rows === 1) throwError("username taken");
+
+$stmt = $conn->prepare("insert into User (Username, Password, Activated) values (?, password(?), 1)");
+$stmt->bind_param("ss", $body->username, $body->password);
 $stmt->execute();
 $result = $stmt->get_result();
 if($result->num_rows === 0) throwError("no success");
 
-$userID = $conn->insert_id;
+/*$userID = $conn->insert_id;
 $stmt = $conn->prepare("insert into UserActivation (UserID, RegistrationKey) values (?, uuid())");
 
 $stmt->bind_param("i", $userID);
@@ -46,7 +53,7 @@ https://philcomm.dev/servicemarket/api/auth/activate_user.php?activation_code=$u
 $msg = wordwrap($msg,70);
 
 // send email
-mail($body->email, "PokeMMO ServiceMarket Account Verification", $msg);
+mail($body->email, "PokeMMO ServiceMarket Account Verification", $msg);*/
 
 $stmt->close();
 
